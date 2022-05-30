60f7f686ee7e04b997295b0b3032a4a78036e79e
Tiago Souza <tiago.souza@havan.com.br>
2022-05-30 11:48:39 -0300

    CrudTasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle){
+      alert('Insira o nome da task.')
+      return;
+    }
+
+    const newTask:Task = {
+      id:  Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [ ...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksonToogle = tasks
+      .map(task => task.id === id ? 
+        { ...task, isComplete: !task.isComplete} : 
+        task)
+
+      setTasks(tasksonToogle);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id != id));
   }
 
   return (
